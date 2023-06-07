b0ac116cfbda0d8aa3276d735a176130f6571a1e
Novvator <stamatis.sellas@yahoo.com>
2023-06-07 20:44:26 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="bg">
+      <div class="container">
+        <h1>Welcome to CineMatch!</h1>
+        <p>Please enter your Letterboxd/IMDB username to get movie recommendations.</p>
+        <div class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" id="username" [(ngModel)]="username" class="form-control">
+        </div>
+        <button (click)="getMovieRecommendations()" class="btn btn-primary">Get Recommendations</button>
+
+        <div class="movie-list" *ngIf="recommendations.length > 0">
+          <h2>Movie Recommendations for {{ username }}:</h2>
+          <div class="movie" *ngFor="let movie of recommendations">
+            <img [src]="movie.imageUrl" alt="{{ movie.title }}">
+            <h3>{{ movie.title }}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cine-match-app';
+  username: string = '';
+  recommendations: Movie[] = [];
+
+  getMovieRecommendations() {
+    // Simulate API call to fetch movie recommendations based on the username
+    // Replace this with your actual API call implementation
+    // For demonstration purposes, we'll use a sample set of movies
+    this.recommendations = [
+      { title: 'BeetleJuice', imageUrl: 'assets/images/movie1.jpg' },
+      { title: 'BladeRunner', imageUrl: 'assets/images/movie2.jpg' },
+      { title: 'Friday 13th', imageUrl: 'assets/images/movie3.jpg' },
+      { title: 'Pulp Fiction', imageUrl: 'assets/images/movie4.jpg' },
+    ];
+  }
+}
+
+interface Movie {
+  title: string;
+  imageUrl: string;
 }
